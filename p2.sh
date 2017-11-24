c45cb4ac4dc1b3e98f9b54a8cfc4ae7a3c5ddc09
Shardul <shardul@ualberta.ca>
2017-11-24 00:28:05 -0700

    Bug fixing for BASH script

--- p2.sh
+++ p2.sh
@@ -6,16 +6,17 @@ sort -u -o terms.txt terms.txt
 sort -u -o recs.txt recs.txt
 sort -u -o years.txt years.txt
 
-perl break.pl <terms.txt >terms.txt
-perl break.pl <recs.txt >recs.txt
-perl break.pl <years.txt >years.txt
+# can't output the same file name as in the input file name in perl commands like these, or the output file gets written blank (not sure why; but output file name must be different from input file name
+perl break.pl <terms.txt >terms_o.txt 
+perl break.pl <recs.txt >recs_o.txt
+perl break.pl <years.txt >years_o.txt
 
 
-db_load -T -t hash -f recs.txt recs.db
-db_load -T -t btree -f terms.txt terms.db
-db_load -T -t btree -f years.txt years.db
+db_load -T -t hash -f recs_o.txt re.idx
+db_load -T -t btree -f terms_o.txt te.idx
+db_load -T -t btree -f years_o.txt ye.idx
 
-# Use the following command to test/show the database as terminal output. Replace db_name.db with the database name:
-#db_dump -p db_name.db
+# Use the following command to test/show the database as terminal output. Replace index_name.idx with the index name:
+#db_dump -p index_name.idx
 
 
